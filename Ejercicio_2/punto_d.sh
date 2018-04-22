165aefc563bf4492ee3e5be16bfb916d6d8206cf
root <root@debian.debian@mozart>
2018-04-22 00:04:03 -0300

    punto_d proceso en background

--- Ejercicio_2/punto_d.sh
+++ Ejercicio_2/punto_d.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-read -p "Ingrese el proceso a filtrar: " proceso
+#ejecuta estadoDeProceso en segundo plano
+./punto_d/estadoDeProceso&
 
-$proceso&
+#guarda en $id el id del ultimo proceso ejecutado en segundo plano
+id=$!
 
-PID=$!
+#se filtra top mediante grep para ver solo el proceso con el id guardado
+#top | grep $id 
 
-top | grep $PID
+#kill $id
+
+for (( i=5; i>0; i-- ));do
+	top | grep $id 
+	sleep 5
+done
+
+kill $id
+
+
+
