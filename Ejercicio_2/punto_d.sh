9debbd5f3f126dae03943c014ef1da52388bb504
luciano <karlux@debian.debian@mozart>
2018-04-26 13:47:51 -0300

    se corrigio el punto d, ahora sale cuando termina

--- Ejercicio_2/punto_d.sh
+++ Ejercicio_2/punto_d.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ./Ejercicio_2/bucle.sh &  #Se ejecuta en segundo plano
-top | grep $! #Se filtra el id del proceso en top
+top -d 2 -n 12 | grep $! #Se filtra el id del proceso en top
+						 #con un delay de 2 segundos cada actualizacion
+						 #y se muestra las veces indicadas en -n
+
 
 
