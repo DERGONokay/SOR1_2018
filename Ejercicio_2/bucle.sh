70558d38a4271dc9c9888449536851ae78391ddd
luciano <karlux@debian.debian@mozart>
2018-04-26 11:43:43 -0300

    borrado de echos en bucle.sh por merge conflict

--- Ejercicio_2/bucle.sh
+++ Ejercicio_2/bucle.sh
@@ -3,24 +3,14 @@ tiempo=6
 cont=0
 clear
 while [[ $cont -lt $tiempo ]];do
-	##############################
-	echo ""
-	echo "======================================================================================"
-	echo ""
-	##############################
-	echo "ejecutando------>R"
+	
 	for(( i=0; i<100000;i++ ));do
 		echo $i >> temporal.txt
 	done
 	rm temporal.txt	
-	##############################
-	echo "durmiendo------->S"
+	
 	sleep 3
-	##############################
+
 	let cont=$cont+1
-	##############################
+	
 done
-echo ""
-echo "=========================================="
-echo " Finalizado, presiones ctrl+c para salir"
-echo "=========================================="
