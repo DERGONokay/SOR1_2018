d41d34180f22938147b39304bbfbcc6f9e6ab563
damian <damian.lisas@movasim.com>
2018-04-26 15:38:00 -0300

    "Actualizada la funcion punto_d.sh"

--- Ejercicio_2/punto_d.sh
+++ Ejercicio_2/punto_d.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-./Ejercicio_2/bucle.sh &  #Se ejecuta en segundo plano
-top | grep $! #Se filtra el id del proceso en top
+~/SOR1_2018/Ejercicio_2/bucle.sh &  #Se ejecuta en segundo plano
+PROCID=$!
+A=0
 
+while true ; do
+	top -bn1 | grep $PROCID #Se filtra el id del proceso en top
+	if [[ $A -eq 2 ]]; then
+		exit 0
+	fi
+	sleep 3
+	A=$A+1
+done
 
